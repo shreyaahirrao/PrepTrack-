848a01a81c8b8395483105b73480ba5665fd1054
shreyaahirrao <shreyaahirrao06@gmail.com>
2026-09-24 10:41:36 +0530

    Fix cross-origin cookies for production

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -8,7 +8,7 @@ const setTokenCookie = (res, token) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
